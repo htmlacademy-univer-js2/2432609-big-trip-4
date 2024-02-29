42fb2076d0291be43b2a3531270e5503622abc61
ssftvyn <kutabare.wa@gmail.com>
2024-02-29 16:03:44 +0500

    настройка

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '' },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+  ],
+  devtool: 'source-map',
+};
