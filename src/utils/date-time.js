7caad70921fed1b40e21bd3d17d5dfcb61792ab3
ssftvyn <kutabare.wa@gmail.com>
2024-05-16 20:42:24 +0500

    sort

--- src/utils/date-time.js
+++ src/utils/date-time.js
@@ -3,11 +3,11 @@ import duration from 'dayjs/plugin/duration';
 
 dayjs.extend(duration);
 
-const humanizeDate = (date) => dayjs(date).format('D MMMM');
+export const humanizeDate = (date) => dayjs(date).format('D MMMM');
 
-const humanizeTime = (date) => dayjs(date).format('HH:mm');
+export const humanizeTime = (date) => dayjs(date).format('HH:mm');
 
-const getEventDuration = (dateFrom, dateTo) => {
+export const getEventDuration = (dateFrom, dateTo) => {
   const timeParts = dayjs.duration(dayjs(dateTo).diff(dateFrom)).
     format('DD HH mm').
     split(' ');
@@ -28,11 +28,45 @@ const getEventDuration = (dateFrom, dateTo) => {
   return eventDuration;
 };
 
-const humanizeFormDate = (date) => dayjs(date).format('DD/MM/YY HH:mm');
+const getWeightForNullDate = (dateA, dateB) => {
+  if (dateA === null && dateB === null) {
+    return 0;
+  }
 
-const isPastPoint = (point) => dayjs(point.dateFrom).isBefore(dayjs());
+  if (dateA === null) {
+    return 1;
+  }
 
-const isFuturePoint = (point) => dayjs(point.dateFrom).isAfter(dayjs());
+  if (dateB === null) {
+    return -1;
+  }
 
-export{ humanizeDate, humanizeTime, humanizeFormDate, getEventDuration,
-  isPastPoint, isFuturePoint };
+  return null;
+};
+
+export const getWrightForTwoNullDates = (pointA, pointB) => {
+  const weightA = getWeightForNullDate(pointA.dateFrom, pointA.dateTo);
+  const weightB = getWeightForNullDate(pointB.dateFrom, pointB.dateTo);
+
+  return weightA && weightB;
+};
+
+export const sortByDay = (pointA, pointB) => {
+  const weight = getWeightForNullDate(pointA.dateFrom, pointB.dateFrom);
+
+  return weight ?? dayjs(pointA.dateFrom).diff(dayjs(pointB.dateFrom));
+};
+
+export const sortByTime = (pointA, pointB) => {
+  const timeA = dayjs(pointA.dateTo).diff(dayjs(pointA.dateFrom));
+  const timeB = dayjs(pointB.dateTo).diff(dayjs(pointB.dateFrom));
+  return timeA - timeB;
+};
+
+export const sortByPrice = (pointA, pointB) => pointA.basePrice - pointB.basePrice;
+
+export const humanizeFormDate = (date) => dayjs(date).format('DD/MM/YY HH:mm');
+
+export const PastPoint = (point) => dayjs(point.dateFrom).isBefore(dayjs());
+
+export const FuturePoint = (point) => dayjs(point.dateFrom).isAfter(dayjs());
--- src/view/sort.js
+++ src/view/sort.js
@@ -1,10 +1,11 @@
 import AbstractView from '../framework/view/abstract-view.js';
+import { SortType } from '../const.js';
 
 const createSortingTemplate = () => (
   `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-  <div class="trip-sort__item  trip-sort__item--day">
-  <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day">
-  <label class="trip-sort__btn" for="sort-day">Day</label>
+  <div class="trip-sort__item  trip-sort__item--${SortType.DAY}">
+  <input id="sort-${SortType.DAY}" data-sort-type=${SortType.DAY} class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${SortType.DAY}" checked>
+  <label class="trip-sort__btn" for="sort-${SortType.DAY}">Day</label>
   </div>
 
   <div class="trip-sort__item  trip-sort__item--event">
@@ -12,14 +13,14 @@ const createSortingTemplate = () => (
   <label class="trip-sort__btn" for="sort-event">Event</label>
   </div>
 
-  <div class="trip-sort__item  trip-sort__item--time">
-  <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-  <label class="trip-sort__btn" for="sort-time">Time</label>
+  <div class="trip-sort__item  trip-sort__item--${SortType.TIME}">
+  <input id="sort-${SortType.TIME}" data-sort-type=${SortType.TIME} class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${SortType.TIME}">
+  <label class="trip-sort__btn" for="sort-${SortType.TIME}">Time</label>
   </div>
 
-  <div class="trip-sort__item  trip-sort__item--price">
-  <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" checked>
-  <label class="trip-sort__btn" for="sort-price">Price</label>
+  <div class="trip-sort__item  trip-sort__item--${SortType.PRICE}">
+  <input id="sort-${SortType.PRICE}" data-sort-type=${SortType.PRICE} class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${SortType.PRICE}">
+  <label class="trip-sort__btn" for="sort-${SortType.PRICE}">Price</label>
   </div>
 
   <div class="trip-sort__item  trip-sort__item--offer">
@@ -33,4 +34,17 @@ export default class SortingView extends AbstractView {
   get template () {
     return createSortingTemplate();
   }
+
+  setSortTypeChangeHandler = (callback) => {
+    this._callback.sortTypeChange = callback;
+    this.element.addEventListener('click', this.#sortTypeChangeHandler);
+  };
+
+  #sortTypeChangeHandler = (evt) => {
+    if (evt.target.tagName !== 'INPUT') {
+      return;
+    }
+
+    this._callback.sortTypeChange(evt.target.dataset.sortType);
+  };
 }
